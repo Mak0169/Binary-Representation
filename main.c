4b88018fe7d467c6a319693e97a259c8a0597e07
Mak0169 <mak.gumpf@gmail.com>
2026-01-27 22:52:36 -0500

    works with 4 bit integers

--- main.c
+++ main.c
@@ -1,4 +1,16 @@
-int main() {
+#include <stdio.h>
+#include <stdlib.h>
 
-   
+void getBinary(unsigned x) {
+    for (int i = 3; i >= 0; --i) {
+        printf("%d", (x >> i) & 1);
+    }
+}
+
+int main() {
+    printf("Type a character to figure out what it is in binary: ");
+    unsigned int number;
+    scanf("%d", &number);
+    getBinary(number);
+    printf("\n");
 }

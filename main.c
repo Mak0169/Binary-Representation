4b32bb4b0a81a096011a2b1fb49c890435a6055e
Mak0169 <mak.gumpf@gmail.com>
2026-01-27 23:23:01 -0500

    fixed a few things

--- main.c
+++ main.c
@@ -2,15 +2,28 @@
 #include <stdlib.h>
 
 void getBinary(unsigned x) {
-    for (int i = 3; i >= 0; --i) {
+    for (int i = 31; i >= 0; --i) {
         printf("%d", (x >> i) & 1);
     }
 }
 
 int main() {
-    printf("Type a character to figure out what it is in binary: ");
-    unsigned int number;
-    scanf("%d", &number);
-    getBinary(number);
+    int var = 0;
+    unsigned int num = 0;
+    printf("1. Convert integer to binary\n");
+    printf("2. Convert character to binary\n");
+    printf("Choose an option: ");
+    scanf("%d", &var);
+    
+    switch (var) {
+    case 1:
+        printf("Enter number: ");
+        scanf("%d", &num);
+        getBinary(num);
+        break;
+    case 2:
+        break;
+    }
     printf("\n");
+    return 0;
 }

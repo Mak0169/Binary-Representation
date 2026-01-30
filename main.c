4a19eed0093b4edc2e03207e3d5935623b4f6071
Mak0169 <mak.gumpf@gmail.com>
2026-01-29 23:34:06 -0500

    Not finished but I'm tired and want to go to bed

--- main.c
+++ main.c
@@ -1,7 +1,7 @@
 #include <stdio.h>
 #include <stdlib.h>
 
-void getBinary(unsigned x) {
+void getBinaryInt(unsigned int x) {
     if (x < 16) {
         for (int i = 3; i >= 0; --i) {
             printf("%d", (x >> i) & 1);
@@ -17,9 +17,12 @@ void getBinary(unsigned x) {
     }
 }
 
+void getCharBinary(unsigned char ch)
+
 int main() {
     int var = 0;
     unsigned int num = 0;
+    unsigned char ch;
     printf("1. Convert integer to binary\n");
     printf("2. Convert character to binary\n");
     printf("Choose an option: ");
@@ -29,9 +32,11 @@ int main() {
     case 1:
         printf("Enter number: ");
         scanf("%d", &num);
-        getBinary(num);
+        getBinaryInt(num);
         break;
     case 2:
+        printf("Enter: ");
+        ch = getchar();
         break;
     }
     printf("\n");
